9983a308653f9681098725c92cdb057fe4142f4f
CsDenes <Denes@Denes-MacBook-Pro.local>
2016-04-07 23:18:32 +0200

    json

--- index.js
+++ index.js
@@ -6,12 +6,8 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 1337;
-var jsonobj = [
-    { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
-    { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
-    { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
-    { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
-];
+var jsonobj = [];
+
 
 
 var mysql      = require('mysql');
@@ -29,11 +25,12 @@ app.get('/', function(req, res) {
         connection.end();
         if (!err)
             console.log('The solution is: ', rows);
+            jsonobj=rows;
         else
             console.log('Error while performing Query.');
     });
 
-    res.json(rows);
+    res.json(jsonobj);
 
 });
 
