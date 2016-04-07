65a68974f26a238cdfb0bfcb8d96880e6e4e247a
CsDenes <Denes@Denes-MacBook-Pro.local>
2016-04-07 17:57:23 +0200

    com

--- server.js
+++ server.js
@@ -1,6 +1,48 @@
-var http = require('http')
+var express = require('express');
+var app = express();
+
+
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
+
+//app.set('view engine', 'ejs');
+
+//Serve static before session
+app.use(express.static('public'));
+
+
+/**
+ * Let's creat the .tpl and .error on the res object
+ */
+app.use(function (req, res, next) {
+    res.tpl = {};
+    res.tpl.error = [];
+    console.log("Hello")
+    return next();
+});
+
+
+/**
+ * Standard error handler
+ */
+
+
+
+app.get('/', function(req,res){
+   // res.json({foo:'bartest'})
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(port);
+})
+
+app.use(function (err, req, res, next) {
+    res.status(500).send('Houston, we have a problem!');
+
+    //Flush out the stack to the console
+    console.error(err.stack);
+});
+
+var server = app.listen(port, function () {
+
+    console.log('Hello : 1337');
+});/**
+ * Created by Denes on 07/04/16.
+ */
